7a71b1f5d75f62f52ceae77583022d3674cd02d2
mpyw <ryosuke_i_628@yahoo.co.jp>
2019-11-03 16:09:59 +0900

    Implement support for AnonymousResourceCollection

---
+++ src/Http/Resources/Json/MakesAnonymousPaginationResultAwareResourceCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lampager\Laravel\Http\Resources\Json;
+
+/**
+ * Trait MakesAnonymousPaginationResultAwareResourceCollection
+ *
+ * @mixin \Illuminate\Http\Resources\Json\JsonResource
+ */
+trait MakesAnonymousPaginationResultAwareResourceCollection
+{
+    /**
+     * Create new anonymous resource collection.
+     *
+     * @param  mixed                                                       $resource
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public static function collection($resource)
+    {
+        return tap(new AnonymousPaginationResultAwareResourceCollection($resource, static::class), function ($collection) {
+            if (property_exists(static::class, 'preserveKeys')) {
+                $collection->preserveKeys = (new static([]))->preserveKeys === true;
+            }
+        });
+    }
+}
