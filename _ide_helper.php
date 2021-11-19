fee206854a4c6902e9d798496ed12a7a99a32f95
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-11-19 15:02:22 +0900

    feat: 🎸 Add _ide_helper.php

---
+++ _ide_helper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Illuminate\Database\Eloquent
+{
+    use Lampager\Laravel\Paginator;
+
+    if (false) {
+        class Builder
+        {
+            /**
+             * @see \Lampager\Laravel\MacroServiceProvider
+             */
+            public function lampager(): Paginator
+            {
+            }
+        }
+    }
+}
+
+namespace Illuminate\Database\Eloquent\Relations
+{
+    use Lampager\Laravel\Paginator;
+
+    if (false) {
+        class Relation
+        {
+            /**
+             * @see \Lampager\Laravel\MacroServiceProvider
+             */
+            public function lampager(): Paginator
+            {
+            }
+        }
+    }
+}
+
+namespace Illuminate\Database\Query
+{
+    use Lampager\Laravel\Paginator;
+
+    if (false) {
+        class Builder
+        {
+            /**
+             * @see \Lampager\Laravel\MacroServiceProvider
+             */
+            public function lampager(): Paginator
+            {
+            }
+        }
+    }
+}
